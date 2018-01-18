db2c6e2057f1d7db10e1302533ddc9f4e2fb7781
rina123 <rinafahridawati@yahoo.com>
2018-01-18 21:38:46 +0800

    disini saya mengisi data yang akan di tampilkan

--- tugas.cpp
+++ tugas.cpp
@@ -12,3 +12,17 @@ private:
     string jenisular;
     int panjangular;
     int panjangtaring;
+    
+public:
+    Speciesular (string namaular, string namailmiah, string habitatular, string warnasisikular, string jenisular, 
+	int panjangular, int panjangtaring)
+    {
+    	Speciesular::namaular 		= namaular;
+    	Speciesular::namailmiah 			= namailmiah;
+        Speciesular::habitatular 		= habitatular;
+        Speciesular::warnasisikular 	= warnasisikular;
+        Speciesular::jenisular 		= jenisular;
+        Speciesular::panjangular 			= panjangular;
+        Speciesular::panjangtaring 		= panjangtaring;
+    }
+
