8ca6b13c8c11ac965d868b774c3c80fef3d2f911
rina123 <rinafahridawati@yahoo.com>
2018-01-18 21:42:29 +0800

    disini saya membuat variabel void untuk menampilkan hasil

--- tugas.cpp
+++ tugas.cpp
@@ -25,4 +25,19 @@ public:
         Speciesular::panjangular 			= panjangular;
         Speciesular::panjangtaring 		= panjangtaring;
     }
+    
+void Output (void)
+    {
+    	cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+    	cout << "+++++++++++++++++++++++++++++++++SPECIES ULAR++++++++++++++++++++++++++++++++++"<< endl;
+        cout << "Nama ular           	: " << namaular << endl;
+        cout << "Nama Ilmiah           	: " << namailmiah << endl;
+        cout << "Habitat ular       	: " << habitatular << endl;
+        cout << "Warna sisik ular     	: " << warnasisikular << endl;
+        cout << "Jenis ular      	: " << jenisular << endl;
+        cout << "panjang ular      	: " << panjangular << " Meter" << endl;
+        cout << "panjang taring  		: " << panjangtaring << " CM" <<endl;
+        cout << endl;
+    };
+};
 
